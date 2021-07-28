f11b0794f6c34c68f9108a5e27747f7bef1b85b5
EspiraMarvin <espiramarvin@gmail.com>
2021-07-28 22:03:29 +0300

    feat: adds user details for login

---
+++ src/usersDetails/users.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    'name': 'espira',
+    'email':  'espira@gmail.com',
+    'role': 'admin'
+  },
+  {
+    'name': 'marvin',
+    'email':  'marvin@gmail.com',
+    'role': 'normal'
+  }
+]
