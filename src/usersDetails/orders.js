9b499a32eeaba454a4897326525604808a29b879
EspiraMarvin <espiramarvin@gmail.com>
2021-07-29 17:25:45 +0300

    feat: added data for reorders

---
+++ src/usersDetails/orders.js
@@ -0,0 +1,54 @@
+import {uid} from "quasar";
+export const orders =
+[
+  {
+    id: uid(),
+    name: "Coffee",
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Fruits',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Nuts',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Cheese',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Eggs',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Wheat',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Bread',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Sugar',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Barley',
+    status: false
+  },
+  {
+    id: uid(),
+    name: 'Biscuits',
+    status: false
+  }
+]
